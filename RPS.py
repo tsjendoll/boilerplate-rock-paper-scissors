ca8e4ad69cca0456c2b60c150d7187b32e9a9bd1
Jen Berenguel <joshberenguel@gmail.com>
2024-02-16 02:08:41 +0000

    First Commit

--- RPS.py
+++ RPS.py
@@ -1,10 +1,54 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+steps = {}
 
+
+# the strategy is similar to abbey, but we look backs harder than her.
+# she only look back 2 steps, find most frequently pattern of all 2 moves,
+#
+# Other strategies:
+#
+# - quincy repeat 5 moves
+# - kris always counter our last moves, hence, once we establed a patterns, he
+# is not a problem
+# - mrugresh look for our top pick in last 10 moves, hence, similar to kris,
+# once we establed a pattern, we're in control.
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play != "":
+        opponent_history.append(prev_play)
+
+    # Interestingly, 3 to 6 works best, as in we win more than 60%.
+    # If n is larger than 6, we start to get terrible result.
+    # I guess it's becauase we don't have enough data to predict once n get that
+    # larger, we only play 1000 games.
+    n = 3
+
+    hist = opponent_history
 
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(hist) > n:
+        pattern = join(hist[-n:])
+
+        if join(hist[-(n + 1):]) in steps.keys():
+            steps[join(hist[-(n + 1):])] += 1
+        else:
+            steps[join(hist[-(n + 1):])] = 1
+
+        possible = [pattern + "R", pattern + "P", pattern + "S"]
+
+        for i in possible:
+            if not i in steps.keys():
+                steps[i] = 0
+
+        predict = max(possible, key=lambda key: steps[key])
+
+        if predict[-1] == "P":
+            guess = "S"
+        if predict[-1] == "R":
+            guess = "P"
+        if predict[-1] == "S":
+            guess = "R"
 
     return guess
+
+
+def join(moves):
+    return "".join(moves)
